17e9053c00eda21c3d5d8828a1875ade410345d2
Antal <antal.koti@pm.me>
2023-06-22 16:11:16 +0200

    update 5 files

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# Usage: 
+# Usage:
 Accepting all outgoing trade confirmations, use this wisely
 Use with your own risks.
 
-## Installation: 
+## Installation:
 
 1. Install Node LTS;
 2. Edit your own .env.example file; delete the .example extension afterwards.
 3. Run CLI command on the folder that you cloned the source code: ```npm install```
 4. Initiate the code with CLI command: ```node index```
+
+## Extraction secrets from .maFile
+
+- go to your SDA directory
+- open your .maFile (it will be 7656______.maFile somewhere)
+- copy the required tokens to the created .env file
+- start
+
+
+If your SDA is encrypted, you should decrypt it first.
--- index.js
+++ index.js
@@ -5,56 +5,60 @@ const steam = new SteamCommunity();
 require("dotenv").config();
 
 const logOnOptions = {
-  accountName: process.env.STEAM_USERNAME,
-  password: process.env.PASSWORD,
-  twoFactorCode: SteamTotp.getAuthCode(process.env.SHARED_SECRET),
+	accountName: process.env.STEAM_USERNAME,
+	password: process.env.STEAM_PASSWORD,
+	twoFactorCode: SteamTotp.getAuthCode(process.env.SDA_SHARED_SECRET),
 };
 
-const pollingInterval = 30000;
-
 steam.login(logOnOptions, function (err) {
-  if (err) {
-    // It's better to know what's happening
-    console.log(`Error logging in with steam\n ${err}`);
-    throw err;
-  }
+	if (err) {
+		console.log(`Error during login: ${err.message}`);
+		throw err;
+	}
 
-  console.log(`Successfully loggedin as ${process.env.STEAM_USERNAME}`);
-  setInterval(() => {
-    const time = SteamTotp.time();
-    const confKey = SteamTotp.getConfirmationKey(
-      process.env.IDENTITY_SECRET,
-      time,
-      "conf"
-    );
-    const allowKey = SteamTotp.getConfirmationKey(
-      process.env.IDENTITY_SECRET,
-      time,
-      "allow"
-    );
+	console.log(`Successfully logged in as ${process.env.STEAM_USERNAME}`);
+	setInterval(() => {
+		const time = SteamTotp.time();
+		const confKey = SteamTotp.getConfirmationKey(
+			process.env.SDA_IDENTITY_SECRET,
+			time,
+			"conf"
+		);
+		const allowKey = SteamTotp.getConfirmationKey(
+			process.env.SDA_IDENTITY_SECRET,
+			time,
+			"allow"
+		);
 
-    steam.getConfirmations(time, confKey, function (err, confirmations) {
-      if (err) {
-        // Tell me whhat happened
-        console.log(`Error during confirmations: ${err.message}`);
-        throw err;
-      }
 
-      if (confirmations && confirmations.length > 0) {
-        console.log(confirmations);
-        steam.acceptAllConfirmations(time, confKey, allowKey, function (err) {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
+		steam.getConfirmations(time, confKey, function (err, confirmations) {
+			if (err) {
+				// Tell me whhat happened
+				console.log(`Error during confirmations: ${err.message}`);
+				throw err;
+			}
 
-          // TODO: Throwing error here have some problem with syncing?
-          // throw err;
-        });
-      } else {
-        console.log(`No active confirmation`);
-      }
-      return;
-    });
-  }, pollingInterval);
+			if (confirmations && confirmations.length > 0) {
+				console.log(`Accepting ${confirmations.length} confirmations...`);
+				for (const confirmation of confirmations) {
+					console.log(confirmation.title);
+					console.log(confirmation.receiving);
+					console.log(confirmation.sending);
+					steam.acceptConfirmationForObject(
+						process.env.SDA_IDENTITY_SECRET,
+						confirmation.offerID,
+						function (err) {
+							console.log(`Accepted confirmation ${confirmation.id}`);
+							if (err) {
+								console.log(err);
+								throw err;
+							}
+						}
+					);
+				}
+			} else {
+				console.log("No confirmations to accept.");
+			}
+		});
+	}, process.env.POLLING_INTERVAL);
 });
